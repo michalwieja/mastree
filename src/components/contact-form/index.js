6885f3767a304387bdf09b2e3ff0587401da6892
mwieja <m.wieja@qarbon.it>
2024-04-09 15:07:54 +0200

    contact

---
+++ src/components/contact-form/index.js
@@ -0,0 +1,85 @@
+'use client'
+
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+
+import styles from './style.module.css'
+
+import { socials } from '@/config'
+
+function ContactForm() {
+  const [loading, setLoading] = useState(false)
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const form = useRef()
+
+  const sendEmail = async (e) => {
+    setLoading(true)
+    e.preventDefault()
+
+    const resetForm = () => {
+      form.current.reset()
+    }
+
+    try {
+      const response = await emailjs.sendForm(
+        process.env.NEXT_PUBLIC_EMAIL_API_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAIL_API_TEMPLATE_ID,
+        form.current,
+        { publicKey: process.env.NEXT_PUBLIC_EMAIL_API_PUBLIC },
+      )
+      console.log('SUCCESS!', response)
+      setSuccessMessage('Email Wysłany. Skontaktujemy się z Tobą!')
+    } catch (error) {
+      console.error('FAILED...', error)
+      setErrorMessage('Nie udało się wysłać wiadomości. Spróbuj ponownie później.')
+    }
+    setLoading(false)
+    resetForm()
+
+    setTimeout(() => {
+      setSuccessMessage('')
+      setErrorMessage('')
+    }, 5000)
+  }
+
+  const phone = socials.find((social) => social.label === 'Phone')
+
+  return (
+    <div className={styles.contactForm}>
+      <div className={styles.address}>
+        <p>Maciej Dybula</p>
+        <p>Katowice, ul. Katowicka 1</p>
+        <p>
+          <a href={phone.url} target='_blank'>{phone.url}</a>
+        </p>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <h3>Formularz kontaktowy</h3>
+        <label htmlFor="user_name" className={styles.label}>
+    Imię i Nazwisko
+        </label>
+        <input type="text" id="user_name" name="user_name" className={styles.inputField} required />
+
+        <label htmlFor="user_email" className={styles.label}>
+    Email
+        </label>
+        <input type="email" id="user_email" name="user_email" className={styles.inputField} required />
+
+        <label htmlFor="message" className={styles.label}>
+    Wiadomość
+        </label>
+        <textarea id="message" name="message" className={styles.inputField} required />
+
+        <button type="submit" className={styles.button} disabled={loading}>
+          {loading ? 'Wysyłanie...' : 'Wyślij'}
+          {successMessage && <p className={styles.successMessage}>{successMessage}</p>}
+          {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+        </button>
+      </form>
+
+    </div>
+  )
+}
+
+export default ContactForm
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,5 +1,6 @@
 import styles from './styles.module.css'
 
+import ContactForm from '@/components/contact-form'
 import HorizontalWrapper from '@/components/horizontal-wrapper'
 
 const Contact = () => {
@@ -7,9 +8,12 @@ const Contact = () => {
 
     <HorizontalWrapper>
       <div className={styles.contact}>
-          mapa
+        <div className={styles.map}>
+          <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=Mastree%20Ko%C5%9Bciuszki%20229%2040%20%E2%80%93%20600%20Katowice+(Mastree)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+          </iframe>
+        </div>
+        <ContactForm/>
       </div>
-      <div>formularz</div>
     </HorizontalWrapper>
 
   )
