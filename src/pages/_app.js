647868cdb324c16d4d0b73ece92add13428240f7
mwieja <m.wieja@qarbon.it>
2024-05-06 14:18:42 +0200

    favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { motion, AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
 
 import Header from '@/components/header'
 import Footer from '@/components/footer'
@@ -8,6 +9,16 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps, router }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon/site.webmanifest"/>
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
+        <meta name="theme-color" content="#ffffff"/>
+      </Head>
       <Header />
       <AnimatePresence mode="wait">
         <motion.div
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, Head,NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="pl">
-      <Head />
+      <Head/>
       <body>
         <Main />
         <NextScript />
