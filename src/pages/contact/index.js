f5e8362232e2f70cd13516e028c7986e7ecc36ff
mwieja <m.wieja@qarbon.it>
2024-05-08 11:54:40 +0200

    contact with photo

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -8,9 +8,10 @@ const Contact = () => {
 
     <HorizontalWrapper>
       <div className={styles.contact}>
-        <div className={styles.map}>
-          <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=Mastree%20Ko%C5%9Bciuszki%20229%2040%20%E2%80%93%20600%20Katowice+(Mastree)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-          </iframe>
+        <div className={styles.photo}>
+          <picture>
+            <img src="/maciek.jpeg" alt="maciek" />
+          </picture>
         </div>
         <ContactForm/>
       </div>
